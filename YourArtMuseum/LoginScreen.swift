2de1898588986c37880635537dcc134d53703e28
Artem Nikitin <itnikitins@gmail.com>
2022-12-24 21:48:44 +0300

    add backgroundimage

---
+++ YourArtMuseum/LoginScreen.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+class LoginScreen: UIView {
+    private let mainBackgroundImage = UIImageView(frame: .zero)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    
+    private func setupView() {
+        mainBackgroundImage.image = UIImage(named: "MainBackgroundImage")
+        mainBackgroundImage.translatesAutoresizingMaskIntoConstraints = false
+        mainBackgroundImage.contentMode = .scaleAspectFill
+        
+        self.addSubview(mainBackgroundImage)
+
+        NSLayoutConstraint.activate([
+            mainBackgroundImage.topAnchor.constraint(equalTo: topAnchor, constant: -50.0),
+            mainBackgroundImage.bottomAnchor.constraint(greaterThanOrEqualTo: bottomAnchor, constant: 50.0),
+            mainBackgroundImage.rightAnchor.constraint(equalTo: rightAnchor, constant: 30.0),
+        ])
+    }
+}
---
+++ YourArtMuseum/MainViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func loadView() {
+        super.loadView()
+        
+        configureTitle()   
+    }
+    
+    private func configureTitle() {
+        
+    }
+}
+
