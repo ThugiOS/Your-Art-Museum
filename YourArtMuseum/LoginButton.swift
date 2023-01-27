dcf94c5425d230adb17a72a48f512f0b10fcf67a
Artem Nikitin <itnikitins@gmail.com>
2023-01-27 17:10:40 +0300

    add extension UIView + Shadow

--- YourArtMuseum/LoginButton.swift
+++ YourArtMuseum/LoginButton.swift
@@ -10,17 +10,14 @@ class LoginButton: UIButton {
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-    }
+    } 
 
     override func awakeFromNib() {
         super.awakeFromNib()
         
         self.backgroundColor = UIColor(named: "LogInColor")
-        
-        self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
-        self.layer.shadowRadius = 4.0
-        self.layer.shadowOffset = CGSize(width: 0.0, height: 4.0)
-        self.layer.shadowOpacity = 1.0
+    
+//        self.addShadow()
         
         self.addTarget(self, action: #selector(tap), for: .touchUpInside)
     }
